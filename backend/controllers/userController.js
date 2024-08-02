483591e7a6829d8fe84033bb4d2f39438edd1721
BrianWahungu <cepteinhawks@gmail.com>
2024-08-02 13:35:08 +0300

    started the user authentication feature

---
+++ backend/controllers/userController.js
@@ -0,0 +1,13 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import validator from "validator"
+
+
+// Login user
+const loginUser = async (req,res) =>{
+
+}
+
+// Register user
+const registerUser =async 
