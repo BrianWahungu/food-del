e00ebb0e07623b362ac1b0434892f384897a56e0
BrianWahungu <cepteinhawks@gmail.com>
2024-08-31 04:59:11 +0300

    Display Orders in Admin Panel Complete

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -74,4 +74,27 @@ const verifyOrder = async (req,res) => {
     }
 }
 
-export {placeOrder,verifyOrder}
+// user orders for front end
+
+const userOrders = async (req,res) =>{
+    try {
+        const orders = await orderModel.find({userId:req.body.userId})
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+// Listing orders for admin panel
+const listOrders = async (req,res) =>{
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})        
+    }
+}
+
+export {placeOrder,verifyOrder,userOrders,listOrders}
